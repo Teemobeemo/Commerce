37220db654970acc8bc384484b9b683c0372a2a8
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-17 07:52:46 +0530

    📝 Add Models and migrations

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,31 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.utils.timezone import now
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    created_user = models.ForeignKey(User,default=1,on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=500)
+    starting_bid = models.PositiveIntegerField(default=0)
+    image_url = models.CharField(max_length=500)
+
+class Bid(models.Model):
+    bidding_user = models.ForeignKey(User,default=1, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing,default=1, on_delete=models.CASCADE)
+    bidding_amount = models.PositiveIntegerField(default=0)
+    created_date = models.DateField(default=now)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,default=1, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing,default=1, on_delete=models.CASCADE)
+
+class WatchList(models.Model):
+    user = models.ForeignKey(User,default=1, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing,default=1, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    listing = models.ForeignKey(Listing,default=1,on_delete=models.CASCADE)
