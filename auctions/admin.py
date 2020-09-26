a7742ec09cfb52ad9e15ba083a51719e95fe148d
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-26 07:40:06 +0530

    ✨ Add Admin page

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Comment,Listing,Bid
 # Register your models here.
+
+admin.site.register(Comment)
+admin.site.register(Listing)
+admin.site.register(Bid)
