2b62f4b21e1d09a800f0e71866d7295c01285bbe
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-28 09:44:36 +0530

    ✨ Style category page Handle list of category

--- auctions/category.py
+++ auctions/category.py
@@ -2,9 +2,20 @@ from typing import List
 from django.shortcuts import render, redirect
 from .models import Listing
 
+CATEGORY_CHOICE = [
+    'Electronics',
+    'Home Appliances',
+    'Food and groceries',
+    'outdoor',
+    'Other'
+]
 
 def category(request):
     category = request.GET.get('c')
+
+    if not category:
+        return render(request,'auctions/category.html',{'categories':CATEGORY_CHOICE})
+
     listings = None
     try:
         listings = Listing.objects.filter(category=category)
